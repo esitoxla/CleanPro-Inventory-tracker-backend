440b9deb7f2bf4e2b9f23966bd5054faf772e39f
Irene Toxla <toxlairene@gmail.com>
2026-02-27 10:02:37 +0000

    added changes in databse config

--- config/db.js
+++ config/db.js
@@ -1,24 +1,44 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+let sequelize;
 
-async function testConnection() {
-    try {
-        await sequelize.authenticate()
-        console.log("myaql connected!")
-    } catch (error) {
-       console.error("Unable to connect to mysql", error) 
-    }
+if (process.env.DATABASE_URL) {
+  // ===== PRODUCTION (RAILWAY) =====
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+  console.log("Using Railway Database...");
+} else {
+  // ===== LOCAL DEVELOPMENT =====
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      logging: false,
+    },
+  );
+  console.log("Using Local Database...");
 }
 
-testConnection()
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("MySQL connected successfully!");
+  } catch (error) {
+    console.error("Unable to connect to MySQL:", error);
+  }
+}
+
+testConnection();
 
 export default sequelize;
