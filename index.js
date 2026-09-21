2443c2a501fed6ccc104fb3f3039dbeacbaa5bfb
Irene Toxla <toxlairene@gmail.com>
2026-09-21 22:21:53 +0000

    added changes in index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://kora-wo-adwuma.netlify.app/"],
+    origin: ["http://localhost:5173", "https://kora-wo-adwuma.netlify.app"],
     credentials: true,
   }),
 );
